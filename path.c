4ad244fd92d6f6c583c6522deea38b3354ada19c
AmrTaman <amrtaman4325@gmail.com>
2023-05-19 02:40:36 +0000

    yes

--- path.c
+++ path.c
@@ -11,13 +11,18 @@ char **path(char **name)
 	char *be;
 	char **paths, **error;
 	int i;
-	char *env_var, *value, *ret;
+	char *env_var, *value, *ret, *full;
+	char slash[10] = "/";
 	char *n;
 
+printf("sss");
+fflush(stdout);
 	if (access(name[0], X_OK) == 0)
 	{
 		return (name);
 	}
+	printf("sss");
+	fflush(stdout);
 	for (i = 0; environ[i] != NULL; i++)
 	{
 		env_var = environ[i];
@@ -27,17 +32,21 @@ char **path(char **name)
 			value = env_var + 5;
 		}
 	}
-	n = strcat("/", name[0]);
+	printf("sss");
+	fflush(stdout);
+	strcat(slash, name[0]);
 	be = strtok(value, ":");
 	while (be)
 	{
-		if (access(strcat(be, n), X_OK) == 0)
+		strcat(be, slash);
+		printf("%s", be);
+		if (access(be, X_OK) == 0)
 		{
-			ret = realloc(name[0], strlen(strcat(be, n)) + 1);
+			ret = realloc(name[0], strlen(be) + 1);
 			i = 0;
-			while (strcat(be, n)[i])
+			while (be[i])
 			{
-				name[0][i] = strcat(be, n)[i];
+				name[0][i] = be[i];
 				i++;
 			}
 			return (name);
