6c3d7f7ad986346bce40c6d22455e76579f7a2b7
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-05-24 00:55:46 +0300

    Update strings_process.c

--- strings_process.c
+++ strings_process.c
@@ -1,3 +1,46 @@
+#include"shell.h"
+/**
+ * _strlen - returns length of a string
+ * @s: the string
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs comparison of two strangs.
+ * @m1: first string
+ * @m2: second string
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *m1, char *m2)
+{
+	while (*m1 && *m2)
+	{
+		if (*m1 != *m2)
+			return (*m1 - *m2);
+		m1++;
+		m2++;
+	}
+	if (*m1 == *m2)
+		return (0);
+	else
+		return (*m1 < *m2 ? -1 : 1);
+}
+
+
+
 /**
  * _strcpy - copies a string
  * @frst: the destination
