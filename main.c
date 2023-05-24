fc9e99620cf1534d6b7d1d518b91fd0762510de5
amr elstohy <amramr2901@gmail.com>
2023-05-24 18:28:20 +0000

    hi

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+/**
+ * main - simpel shell
+ * @ac: number of arguments
+ * @av: array of inputs
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	char *input;
+	char **command;
+	int i, h, count = 1;
+	
+	signal(SIGINT, handler);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("$ ");
+		}
+		if (ac)
+			input = get_input();
+		if (input == NULL)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else if (input[0] == '\0')
+		{
+			free(input);
+			continue;
+		}
+		command = parse_input(input, av[0], count);
+		count++;
+		if (command == NULL)
+			continue;
+		if (fork() == 0)
+			execve(command[0], command, NULL);
+		wait(NULL);
+		h = 0;
+		while (command[h])
+			h++;
+		for (i = 0; i <= h; i++)
+			free(command[i]);
+		free(command);
+		free(input);
+		if (!isatty(STDIN_FILENO))
+			_puts("$ ");
+	}
+	return (0);
+}
