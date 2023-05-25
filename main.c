ba91b03a4151aeaae4071d4a62716080f5367285
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-05-25 03:49:05 +0300

    new

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include "shell.h"
 /**
+ * checker - simpel shell
+ * @input: user input
+ *
+ * Return: 0 or 1
+ */
+int checker(char *input)
+{
+	int h = 0, check = 0;
+	
+	while(input[h])
+	{
+		if (isspace(input[h]))
+			check++;
+		h++;
+	}
+	h--;
+	if (h == check)
+	{
+		free(input);
+		return (1);
+	}
+	return (0);
+}
+/**
  * main - simpel shell
  * @ac: number of arguments
  * @av: array of inputs
@@ -15,26 +39,22 @@ int main(int ac, char **av)
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-		{
 			_puts("$ ");
-		}
 		if (ac)
 			input = get_input();
 		if (input == NULL)
-		{
 			exit(EXIT_SUCCESS);
-		}
 		else if (input[0] == '\0')
 		{
 			free(input);
 			continue;
 		}
+		if (checker(input))
+			continue;
 		command = parse_input(input, av[0], count);
 		count++;
 		if (command == NULL)
-		{
 			continue;
-		}
 		if (fork() == 0)
 			execve(command[0], command, NULL);
 		wait(NULL);
